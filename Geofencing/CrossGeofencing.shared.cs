edc758a8cdbd3e602b163abc46945cf09de2ba62
Eduard Schäfer <eschaefer@easy-smarthome.de>
2018-06-08 12:28:27 +0200

    Add project files.

---
+++ Geofencing/CrossGeofencing.shared.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Plugin.Geofencing
+{
+    /// <summary>
+    /// Cross Geofencing
+    /// </summary>
+    public static class CrossGeofencing
+    {
+        static Lazy<IGeofencing> implementation = new Lazy<IGeofencing>(() => CreateGeofencing(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Gets if the plugin is supported on the current platform.
+        /// </summary>
+        public static bool IsSupported => implementation.Value == null ? false : true;
+
+        /// <summary>
+        /// Current plugin implementation to use
+        /// </summary>
+        public static IGeofencing Current
+        {
+            get
+            {
+                IGeofencing ret = implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IGeofencing CreateGeofencing()
+        {
+#if NETSTANDARD1_0 || NETSTANDARD2_0
+            return null;
+#else
+#pragma warning disable IDE0022 // Use expression body for methods
+            return new GeofencingImplementation();
+#pragma warning restore IDE0022 // Use expression body for methods
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly() =>
+            new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+
+    }
+}
---
+++ Geofencing/IGeofencing.shared.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Geofencing
+{
+    public interface IGeofencing
+    {
+    }
+}
