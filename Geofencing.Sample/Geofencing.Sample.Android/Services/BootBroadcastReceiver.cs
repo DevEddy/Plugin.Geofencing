f94662351572d9197b7bbe6f5d3dd9d2a7ed201b
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2018-08-03 21:06:26 +0200

    [Android] Listen for device reboot and re-register geofences closing #3

---
+++ Geofencing.Sample/Geofencing.Sample.Android/Services/BootBroadcastReceiver.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+
+namespace Geofencing.Sample.Droid.Services
+{
+    [BroadcastReceiver(Exported = false, DirectBootAware = true)]
+    [IntentFilter(new[] { "android.intent.action.BOOT_COMPLETED", "android.intent.action.LOCKED_BOOT_COMPLETED" })]
+    public class BootBroadcastReceiver : BroadcastReceiver
+    {
+        static readonly string TAG = "BootBroadcastReceiver";
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            bool bootCompleted;
+            string action = intent.Action;
+            Log.Info(TAG, $"Recieved action {action}, user unlocked: "); 
+
+            if (Build.VERSION.SdkInt > BuildVersionCodes.M)
+                bootCompleted = Intent.ActionLockedBootCompleted == action;
+            else
+                bootCompleted = Intent.ActionBootCompleted == action;
+
+            if (!bootCompleted)
+                return;
+
+            GeofencingHelper.StartGeofencingService();
+        }
+    }
+}
